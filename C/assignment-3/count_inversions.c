864652be1d439e460593808a9f38fa0ddb5f662f
Vedanshi18 <vedanshi.1886@gmail.com>
2026-07-23 12:42:14 +0530

    Assignment - 3

--- C/assignment-3/count_inversions.c
+++ C/assignment-3/count_inversions.c
@@ -1,28 +1,16 @@
 //Count inversions in an array
-// Count significant reverse pairs in an array
 #include <stdio.h>
 #include <stdlib.h>
-int countInversions(int arr[], int low, int mid, int high) {
-    // Start pointer in the right half
-    int right = mid + 1;
-    int count = 0;
-    // Iterate over left elements
-    for (int i = low; i<= mid; i++) {
-        while (right <= high && arr[i] > arr[right]) {
-            right++;
-        }
-        count += right - (mid + 1);
-    }
-    return count;
-}
-void merge (int arr[], int low, int mid, int high) {
+
+int merge (int arr[], int low, int mid, int high) {
     int n = high - low + 1;
     int temp[n];
-    int left = low, right = mid + 1, k = 0;
+    int left = low, right = mid + 1, k = 0, count = 0;
     while (left <= mid && right <= high) {
         if (arr[left] <= arr[right]) {
             temp[k++] = arr[left++];
         } else {
+            count += (mid - left + 1);
             temp[k++] = arr[right++];
         }
     }
@@ -37,6 +25,7 @@ void merge (int arr[], int low, int mid, int high) {
     for (int i = low; i <= high; i++) {
         arr[i] = temp[i - low];
     }
+    return count;
 }
 int mergeSort (int arr[], int low, int high) {
     if (low >= high) return 0;
@@ -44,8 +33,7 @@ int mergeSort (int arr[], int low, int high) {
     int count = 0;
     count += mergeSort(arr, low, mid);
     count += mergeSort(arr, mid + 1, high);
-    count += countInversions(arr, low, mid, high);
-    merge(arr, low, mid, high);
+    count += merge(arr, low, mid, high);
     return count;
 }
 int main() {
